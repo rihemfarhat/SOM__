e0146129177142cd74db41044141c0ac60d8a36a
rihemfarhat <rihemfarhat5@gmail.com>
2024-06-24 18:20:55 +0100

    Update progressbar.py
    fixing syntax errors

--- other/progressbar.py
+++ other/progressbar.py
@@ -165,7 +165,7 @@ class Bar(ProgressBarWidgetHFill):
         self.left = left
         self.right = right
     def _format_marker(self, pbar):
-        if isinstance(self.marker, (str, unicode)):
+        if isinstance(self.marker, str):
             return self.marker
         else:
             return self.marker.update(pbar)
@@ -259,7 +259,7 @@ class ProgressBar(object):
                 r.append(w)
                 hfill_inds.append(i)
                 num_hfill += 1
-            elif isinstance(w, (str, unicode)):
+            elif isinstance(w, str):
                 r.append(w)
                 currwidth += len(w)
             else:
