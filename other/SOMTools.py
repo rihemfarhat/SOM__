dbc9b177896472b936aded6b5e9d7771261a1c6b
rihemfarhat <rihemfarhat5@gmail.com>
2024-06-24 18:15:23 +0100

    Update SOMTools.py
    update to print function

--- other/SOMTools.py
+++ other/SOMTools.py
@@ -317,7 +317,7 @@ def minPath(matrix, gradThreshold=None, startingPoint = None, nsteps=None):
    try:
     i,j = path[r]
    except IndexError:
-    print 'BREAK !!!!!'
+    print ('BREAK !!!!!')
     break
   if (i,j) not in outPath:
    outPath.append((i,j))
@@ -756,7 +756,7 @@ def mcpath(matrix, start, nstep, T=298.0, stop = None, k = None, x_offset=None, 
     if stop == None:
         target = matrix.min()
         stop = scipy.ndimage.minimum_position(matrix)
-        print 'Minimal value for (%d,%d) position'%stop
+        print ('Minimal value for (%d,%d) position'%stop)
         minpos = numpy.asarray(numpy.where(matrix == target)).T
     else:
         minpos = numpy.asarray(stop)
